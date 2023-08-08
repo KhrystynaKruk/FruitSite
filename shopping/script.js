87991c38a1b6d47c5f6c9252b7ae518affcfbcd5
Khrystyna Kruk <krystynakruk0@gmail.com>
2023-08-08 16:32:42 +0200

    responsive

--- shopping/script.js
+++ shopping/script.js
@@ -22,38 +22,45 @@ let products = [
     name: "Mango (per kg)",
     image: "mango.png",
     price: 3,
+    color: "orange",
   },
   {
     id: 2,
     name: "Pineapple (per kg)",
     image: "pineApple.png",
     price: 5,
+    color: "#4B92DB",
   },
   {
     id: 3,
     name: "Dragonfruits (per kg)",
     image: "dragonFruits.png",
     price: 3,
+    color: "#c83a60",
   },
   {
     id: 4,
     name: "Apple (per kg)",
     image: "apple.png",
     price: 1,
+    color: "#fc4a55",
   },
   {
     id: 5,
     name: "Watermelon (per kg)",
     image: "watermelon.png",
     price: 6,
+    color: "#228B22",
   },
   {
     id: 6,
     name: "Fig (per kg)",
     image: "fig.png",
     price: 3,
+    color: "#5E008D",
   },
 ];
+
 let listCards = [];
 function initApp() {
   products.forEach((value, key) => {
@@ -62,9 +69,10 @@ function initApp() {
     newDiv.innerHTML = `
             <img src='img/${value.image}'>
             <div class='title'>${value.name}</div>
-            <div class='price'>${value.price.toLocaleString()}</div>
-            <button onclick='addToCard(${key})'>Add To Card</button>`;
+            <div class='price'>${value.price.toLocaleString()}$</div>
+            <button onclick='addToCard(${key})'>Add To Cart</button>`;
     list.appendChild(newDiv);
+    newDiv.style.backgroundColor = value.color;
   });
 }
 initApp();
